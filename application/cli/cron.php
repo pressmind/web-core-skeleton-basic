b4a723eedf6b58c340b4fba92eafb0af6859d6ce
Michael Graute <m.graute@team-braeutigam.de>
2020-12-11 15:41:19 +0100

    added schedule handler

--- application/cli/cron.php
+++ application/cli/cron.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * to make this schedule handler work add a cronjob to your webservers crontab that executes this file every minute
+ * e.g: * * * * * php /your/install/path/application/cli/cron.php  > /dev/null 2>&1
+ */
+
+use Pressmind\Log\Writer;
+use Pressmind\Scheduler;
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$scheduler = new Scheduler();
+try {
+    $scheduler->walk();
+} catch (Exception $e) {
+    Writer::write($e->getMessage(), Writer::OUTPUT_FILE, 'scheduler', Writer::TYPE_ERROR);
+}
