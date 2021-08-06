e7ebe41af669fefb834f6899c02c3067e60b28a4
michael-graute <m.graute@team-braeutigam.de>
2021-08-06 14:50:36 +0200

    - some minor changes

--- httpdocs/detail.php
+++ httpdocs/detail.php
@@ -1,11 +1,13 @@
 <?php
 error_reporting(1);
 ini_set('display_errors', 1);
+
 use Pressmind\ORM\Object\MediaObject;
 
+$start_time = microtime(true);
 require_once '../application/bootstrap.php';
 $skipCache = isset($_GET['preview']);
-$mediaObject = new MediaObject(intval($_GET['id']), true, $skipCache);
+$mediaObject = new MediaObject(intval($_GET['id']), false, $skipCache);
 ?>
 <!doctype html>
 <html lang="en">
@@ -30,7 +32,16 @@ $mediaObject = new MediaObject(intval($_GET['id']), true, $skipCache);
     //To Render a Media Object, we just need to call the Render function and give the suffix of the render script as parameter
     //In this case the view script with the naming convention <MediaObjectType>_Example.php  (e.g. Reise_Example.php) will be called and rendered.
     //The example view scripts for all media object types can be found in /application/Custom/Views after the install.php script has been executed.
-    echo $mediaObject->render('example', null, ['my_custom_key' => 'this is custom data for the render script', 'second_custom_key' => 'this data can be of any kind (string, array, object)']); ?>
+    try {
+        echo $mediaObject->render('example', null, ['my_custom_key' => 'this is custom data for the render script', 'second_custom_key' => 'this data can be of any kind (string, array, object)']);
+    } catch(Exception $e) {
+        echo "Failed to render Media Object";
+    } ?>
+    <?php $end_time = microtime(true);?>
+    <h4>Total Rendertime:</h4>
+    <pre><?php echo ($end_time - $start_time);?> seconds</pre>
+    <h4>Total Memory Usage</h4>
+    <pre><?php echo(memory_get_usage(true) / (1024*1024));?> MB</pre>
 </div>
 <script src="assets/vendor/jquery/jquery-3.5.1.min.js"></script>
 <script src="assets/vendor/popper/popper-1.16.0.min.js"></script>
