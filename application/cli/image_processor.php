6fb1c1765cf32d551c529447b0bb37afb1c3dcf7
michael-graute <m.graute@team-braeutigam.de>
2021-08-06 14:47:04 +0200

    - added processing of itinerary images

--- application/cli/image_processor.php
+++ application/cli/image_processor.php
@@ -6,6 +6,7 @@ use ImagickException;
 use Pressmind\Image\Processor\Adapter\Factory;
 use Pressmind\Image\Processor\Config;
 use Pressmind\Log\Writer;
+use Pressmind\ORM\Object\Itinerary\Step\DocumentMediaObject;
 use Pressmind\ORM\Object\MediaObject\DataType\Picture;
 
 if(php_sapi_name() == 'cli') {
@@ -59,21 +60,58 @@ foreach ($result as $image) {
             Writer::write('ID ' . $image->getId() . ': ' . $e->getMessage(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_ERROR);
             continue;
         }
-        foreach ($image->sections as $section) {
-            Writer::write('ID ' . $image->getId() . ': Downloading section image from ' . $section->tmp_url, WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
-            try {
-                $binary_section_file = $section->downloadOriginal();
-                Writer::write('ID ' . $image->getId() . ': Creating section image derivatives', WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
-                $section->createDerivative($processor_config, $imageProcessor, $binary_section_file);
-                unset($binary_section_file);
-            } catch (Exception $e) {
-                Writer::write('ID ' . $image->getId() . ': ' . $e->getMessage(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_ERROR);
-                continue;
+        if(isset($image->sections)) {
+            foreach ($image->sections as $section) {
+                Writer::write('ID ' . $image->getId() . ': Downloading section image from ' . $section->tmp_url, WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
+                try {
+                    $binary_section_file = $section->downloadOriginal();
+                    Writer::write('ID ' . $image->getId() . ': Creating section image derivatives', WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
+                    $section->createDerivative($processor_config, $imageProcessor, $binary_section_file);
+                    unset($binary_section_file);
+                } catch (Exception $e) {
+                    Writer::write('ID ' . $image->getId() . ': ' . $e->getMessage(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_ERROR);
+                    continue;
+                }
             }
         }
     }
     unset($binary_image);
 }
+
+try {
+    $itinerary_params = ['download_successful' => 0];
+    /** @var DocumentMediaObject[] $result */
+    $itinerary_result = DocumentMediaObject::listAll($itinerary_params);
+} catch (Exception $e) {
+    Writer::write($e->getMessage(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_ERROR);
+}
+
+Writer::write('Processing ' . count($result) . ' images', WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
+
+foreach ($itinerary_result as $itinerary_image) {
+    $itinerary_binary_image = null;
+    Writer::write('Processing itinerary image ID:' . $itinerary_image->getId(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
+    Writer::write('ID ' . $itinerary_image->getId() . ': Downloading itinerary image from ' . $itinerary_image->tmp_url, WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
+    try {
+        $itinerary_binary_image = $itinerary_image->downloadOriginal();
+    } catch (Exception $e) {
+        Writer::write('itinerary image ID ' . $itinerary_image->getId() . ': ' . $e->getMessage(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_ERROR);
+        continue;
+    }
+    $itinerary_imageProcessor = Factory::create($config['image_handling']['processor']['adapter']);
+    Writer::write('ID ' . $itinerary_image->getId() . ': Creating itinerary derivatives', WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
+    foreach ($config['image_handling']['processor']['derivatives'] as $itinerary_derivative_name => $itinerary_derivative_config) {
+        try {
+            $itinerary_processor_config = Config::create($itinerary_derivative_name, $itinerary_derivative_config);
+            $itinerary_image->createDerivative($itinerary_processor_config, $itinerary_imageProcessor, $itinerary_binary_image);
+        } catch (Exception $e) {
+            Writer::write('itinerary image ID ' . $itinerary_image->getId() . ': ' . $e->getMessage(), WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_ERROR);
+            continue;
+        }
+    }
+    unset($itinerary_binary_image);
+}
+
 Writer::write('Image processor finished', WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
 if(isset($argv[1]) && is_numeric($argv[1])) {
     Writer::write('Updating cache', WRITER::OUTPUT_FILE, 'image_processor', Writer::TYPE_INFO);
